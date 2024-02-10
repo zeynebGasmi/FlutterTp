6cba6037846f36f561b10d6f7faf1a82274d3d2b
zeynebGasmi <zeyneb.gasmi@etu.unice.fr>
2024-02-11 00:38:53 +0100

    update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,10 +3,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'home_page.dart';
 import 'register_page.dart';
-import 'home_page.dart';
-class LoginPage extends StatelessWidget {
+import 'package:tp2/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
+
+/*
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  bool _obscureText = true; // Added boolean variable to control password visibility
 
   @override
   Widget build(BuildContext context) {
@@ -32,12 +40,24 @@ class LoginPage extends StatelessWidget {
             SizedBox(height: 20),
             TextField(
               controller: passwordController,
-              obscureText: true,
+              obscureText: _obscureText, // Use boolean variable to control password visibility
               decoration: InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
                 filled: true,
                 fillColor: Colors.grey[200],
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscureText ? Icons.visibility : Icons.visibility_off, // Toggle icon based on _obscureText value
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    // Toggle the value of _obscureText when the eye icon is pressed
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
               ),
             ),
             SizedBox(height: 20),
@@ -61,6 +81,129 @@ class LoginPage extends StatelessWidget {
                     );
                   } catch (e) {
                     // Handle authentication errors
+                    print('Authentication failed: $e');
+                  }
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistrationPage()),
+                );
+              },
+              child: Text('Don\'t have an account? Register here'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+*/
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home_page.dart';
+import 'register_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool _obscureText = true; // Added boolean variable to control password visibility
+  String _errorMessage = ''; // Added variable to hold authentication error message
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.grey[200],
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: passwordController,
+              obscureText: _obscureText, // Use boolean variable to control password visibility
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.grey[200],
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscureText ? Icons.visibility : Icons.visibility_off, // Toggle icon based on _obscureText value
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    // Toggle the value of _obscureText when the eye icon is pressed
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Visibility(
+              visible: _errorMessage.isNotEmpty, // Show error message only if it is not empty
+              child: Text(
+                _errorMessage,
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+                onPressed: () async {
+                  try {
+                    // Reset error message when attempting login
+                    setState(() {
+                      _errorMessage = '';
+                    });
+
+                    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: emailController.text,
+                      password: passwordController.text,
+                    );
+
+                    // Authentication successful, navigate to the welcome page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => WelcomePage()),
+                    );
+                  } catch (e) {
+                    // Handle authentication errors
+                    setState(() {
+                      _errorMessage = 'Authentication failed: $e'; // Set error message to display on the screen
+                    });
                     print('Authentication failed: $e');
                   }
                 },
--- lib/register_page.dart
+++ lib/register_page.dart
@@ -3,7 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:tp2/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
 import 'login_page.dart';
-import 'package:tp2/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
+
+
 
 class RegistrationPage extends StatefulWidget {
   @override
@@ -12,10 +13,12 @@ class RegistrationPage extends StatefulWidget {
 
 class _RegistrationPageState extends State<RegistrationPage> {
   final FirebaseAuthService _auth = FirebaseAuthService();
-
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  bool _obscureText = true; // Added boolean variable to control password visibility
+  String? _emailErrorText; // Added variable to hold email error message
+  String? _passwordErrorText; // Added variable to hold password error message
 
   @override
   Widget build(BuildContext context) {
@@ -46,18 +49,42 @@ class _RegistrationPageState extends State<RegistrationPage> {
                 border: OutlineInputBorder(),
                 filled: true,
                 fillColor: Colors.grey[200],
+                errorText: _emailErrorText,
               ),
+              onChanged: (value) {
+                setState(() {
+                  _emailErrorText = _validateEmail(value);
+                });
+              },
             ),
             SizedBox(height: 20),
             TextField(
               controller: passwordController,
-              obscureText: true,
+              obscureText: _obscureText, // Use boolean variable to control password visibility
               decoration: InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
                 filled: true,
                 fillColor: Colors.grey[200],
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscureText ? Icons.visibility : Icons.visibility_off, // Toggle icon based on _obscureText value
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    // Toggle the value of _obscureText when the eye icon is pressed
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
+                errorText: _passwordErrorText,
               ),
+              onChanged: (value) {
+                setState(() {
+                  _passwordErrorText = _validatePassword(value);
+                });
+              },
             ),
             SizedBox(height: 20),
             SizedBox(
@@ -88,49 +115,53 @@ class _RegistrationPageState extends State<RegistrationPage> {
     super.dispose();
   }
 
- /* void register() async {
+  String? _validateEmail(String value) {
+    if (value.isEmpty) {
+      return 'Email is required';
+    } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Invalid email format';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String value) {
+    if (value.isEmpty) {
+      return 'Password is required';
+    } else if (value.length < 8) {
+      return 'Password must be at least 8 characters';
+    }
+    return null;
+  }
+
+  void register() async {
     String username = nameController.text;
     String email = emailController.text;
     String password = passwordController.text;
-    User? user = await _auth.signUpWithEmailAndPassword(email, password);
 
-    if (user != null) {
-      print("User is successfully created");
-      Navigator.pushReplacementNamed(context, "/home"); // Use pushReplacementNamed
-    } else {
-      print("Some error happened");
+    try {
+      User? user = await _auth.signUpWithEmailAndPassword(email, password);
+
+      if (user != null) {
+        // Registration successful, show a SnackBar
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('User successfully created!'),
+            duration: Duration(seconds: 3), // Adjust the duration as needed
+          ),
+        );
+
+        // Navigate to the login page after registration
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        );
+      } else {
+        print("Some error happened");
+      }
+    } catch (e) {
+      // Handle registration errors
+      print('Registration failed: $e');
     }
-  }*/
-  void register() async {
-  String username = nameController.text;
-  String email = emailController.text;
-  String password = passwordController.text;
-  
-  try {
-    User? user = await _auth.signUpWithEmailAndPassword(email, password);
-
-    if (user != null) {
-      // Registration successful, show a SnackBar
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('User successfully created!'),
-          duration: Duration(seconds: 3), // Adjust the duration as needed
-        ),
-      );
-
-     
-      // Navigate to the welcome page
-      Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => LoginPage()),
-    );
-    } else {
-      print("Some error happened");
-    }
-  } catch (e) {
-    // Handle registration errors
-    print('Registration failed: $e');
   }
 }
 
-}
